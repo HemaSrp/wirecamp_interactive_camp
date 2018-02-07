50d4ac30ef93f9ccb3c19ad72913e5eaa5c79a6e
Hema <hemasrp175@gmail.com>
2018-02-07 20:17:14 +0530

    Created splash screen and google sign in login screen using firebase

---
+++ app/src/main/java/com/wirecamp/assignment/wirecamp/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.wirecamp.assignment.wirecamp.utils;
+
+/**
+ * Created by hema on 7/2/18.
+ */
+
+public class Constants {
+    public static final String FIREBASE_URL = "INCLUDE YOUR FIREBASE URL HERE";
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
+    public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
+
+}
